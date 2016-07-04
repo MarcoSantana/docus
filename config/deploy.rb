8a009e2bcb3fb801b9e72262055157fafeda9e25
Marco Antonio Santana <marco.santana@gmail.com>
2016-07-04 16:07:27 -0400

    Deploy Message

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,44 +1,7 @@
-# config valid only for current version of Capistrano
-lock '3.5.0'
-
-set :application, 'my_app_name'
-set :repo_url, 'git@example.com:me/my_repo.git'
-
-# Default branch is :master
-# ask :branch, `git rev-parse --abbrev-ref HEAD`.chomp
-
-# Default deploy_to directory is /var/www/my_app_name
-# set :deploy_to, '/var/www/my_app_name'
-
-# Default value for :scm is :git
-# set :scm, :git
-
-# Default value for :format is :airbrussh.
-# set :format, :airbrussh
-
-# You can configure the Airbrussh format using :format_options.
-# These are the defaults.
-# set :format_options, command_output: true, log_file: 'log/capistrano.log', color: :auto, truncate: :auto
-
-# Default value for :pty is false
-# set :pty, true
-
-# Default value for :linked_files is []
-# set :linked_files, fetch(:linked_files, []).push('config/database.yml', 'config/secrets.yml')
-
-# Default value for linked_dirs is []
-# set :linked_dirs, fetch(:linked_dirs, []).push('log', 'tmp/pids', 'tmp/cache', 'tmp/sockets', 'public/system')
-
-# Default value for default_env is {}
-# set :default_env, { path: "/opt/ruby/bin:$PATH" }
-
-# Default value for keep_releases is 5
-# set :keep_releases, 5
-
 # Change these
-server 'http://107.170.81.177/', port: 80, roles: [:web, :app, :db], primary: true
-
-set :repo_url,        'https://github.com/MarcoSantana/docus.git'
+server '107.170.81.177', roles: [:web, :app, :db], primary: true
+#port: your_port_num,
+set :repo_url,        'git@github.com:MarcoSantana/docus.git'
 set :application,     'docus'
 set :user,            'deployer'
 set :puma_threads,    [4, 16]
@@ -47,7 +10,7 @@ set :puma_workers,    0
 # Don't change these unless you know what you're doing
 set :pty,             true
 set :use_sudo,        false
-set :stage,           :develop
+set :stage,           :production
 set :deploy_via,      :remote_cache
 set :deploy_to,       "/home/#{fetch(:user)}/apps/#{fetch(:application)}"
 set :puma_bind,       "unix://#{shared_path}/tmp/sockets/#{fetch(:application)}-puma.sock"
